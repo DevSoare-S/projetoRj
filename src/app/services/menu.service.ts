c84d39c19097e9cd75624066c98c902bdd249ceb
Lucas <lucassoares.dev@hotmail.com>
2025-01-21 06:30:35 -0300

    Refazendo o site, mudando ideias e mais

---
+++ src/app/services/menu.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Menu } from '../models/menu';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MenuService {
+
+  constructor() { }
+
+  getItem(): Array<Menu> {
+    const menu: Array<Menu> = [
+    {
+      name: 'Inicio',
+      url: '#Banner'
+    }
+
+    {
+      name: 'Sobre Mim',
+      url: '#Sobre'
+    }
+
+    {
+      name: 'Catálogo',
+      url: '#catálogo'
+    }
+  ]
+  }
+}
