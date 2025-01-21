d3ee0f01aae0fd6d6a6f3d19edee60a77df150ce
Lucas <lucassoares.dev@hotmail.com>
2025-01-21 06:31:38 -0300

    Ajustando menu service

--- src/app/services/menu.service.ts
+++ src/app/services/menu.service.ts
@@ -13,17 +13,24 @@ export class MenuService {
     {
       name: 'Inicio',
       url: '#Banner'
-    }
+    },
 
     {
       name: 'Sobre Mim',
       url: '#Sobre'
-    }
+    },
 
     {
       name: 'Catálogo',
       url: '#catálogo'
+    },
+
+    {
+      name: 'Serviços',
+      url: 'servicos'
     }
-  ]
+  ];
+
+    return menu;
   }
 }
